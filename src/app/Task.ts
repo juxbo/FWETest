3001223293cf11048eda648a95feb6f338f0e940
Jux <justin.boehm@hotmail.de>
2017-11-08 09:56:53 +0100

    Add other Task fields

--- src/app/Task.ts
+++ src/app/Task.ts
@@ -1,13 +1,26 @@
 export class Task {
   id: string;
   name: string;
+  endTime: Date;
+  startTime: Date;
+  created_on: Date;
   done: boolean;
+  description: string;
+  location: string;
+
   edit: boolean;
 
-  constructor(id: string, name: string, done: boolean, edit: boolean = false) {
+
+  constructor(id: string, name: string, endTime: Date, startTime: Date, created_on: Date, done: boolean,
+              description?: string, location?: string, edit: boolean = false) {
     this.id = id;
     this.name = name;
+    this.endTime = endTime;
+    this.startTime = startTime;
+    this.created_on = created_on;
     this.done = done;
+    this.description = description;
+    this.location = location;
     this.edit = edit;
   }
 
--- src/app/tasks/mock-tasks.ts
+++ src/app/tasks/mock-tasks.ts
@@ -1,19 +1,12 @@
 import {Task} from '../task';
 
-// export const TASKS: Task[] = [
-//   {id: '16', name: 'RubberMan', done: false, edit: false},
-//   {id: '17', name: 'Dynama', done: false, edit: false},
-//   {id: '18', name: 'Dr IQ', done: false, edit: false},
-//   {id: '19', name: 'Magma', done: false, edit: false},
-//   {id: '20', name: 'Tornado', done: true, edit: false}
-// ];
+// id: string, name: string, endTime: Date, startTime: Date, created_on: Date, done: boolean,
+//   description?: string, location?: string, edit: boolean = false
 export const TASKS: Task[] = [
-  new Task('16', 'Waschen', false),
-  new Task('17', 'Buegeln', false),
-  new Task('18', 'Lernen', false),
-  new Task('18', 'Lernen', false),
-  new Task('18', 'Lernen', false),
-  new Task('18', 'Lernen', false),
-  new Task('18', 'Lernen', false),
-  new Task('19', 'Duschen', false)
+  new Task('16', 'Testen', new Date(), new Date(), new Date(), false),
+  new Task('17', 'Fixen', new Date(), new Date(), new Date(), false),
+  new Task('18', 'Weinen', new Date(), new Date(), new Date(), false),
+  new Task('19', 'Fixen', new Date(), new Date(), new Date(), false),
+  new Task('20', 'Lachen', new Date(), new Date(), new Date(), false, 'Nun darf gelacht werden', 'Zu Hause')
+  new Task('21', 'Noch mehr weinen', new Date(), new Date(), new Date(), false, 'Es darf nicht mehr gelacht werden. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 'Zu Hause')
 ];
